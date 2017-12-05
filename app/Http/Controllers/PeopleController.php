37e06b458d8ca0f40069d25e7e4ad39e302ba72d
Daniel Rodrigues (geekcom) <danielrodrigues-ti@hotmail.com>
2017-12-05 13:46:02 -0300

    initial commit

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\People;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class PeopleController extends Controller
+{
+    public function show($id)
+    {
+        $people = People::find($id);
+
+        if (count($people)) {
+            return response()->json(['status' => 'success', 'data' => ['people' => $people]], 200);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'no data'], 404);
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|unique:people',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => [
+                    'name' => 'required',
+                    'email' => 'required|unique',
+                ]], 400);
+        }
+
+        $create = People::create($data);
+
+        if (count($create)) {
+            return response()->json(['status' => 'success'], 201);
+        }
+
+        return response()->json(['status' => 'error'], 500);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $people = People::find($id);
+
+        if (count($people)) {
+            $data = $request->all();
+            $validator = Validator::make($data, [
+                'name' => 'sometimes|required',
+                'email' => [
+                    'sometimes',
+                    'required',
+                    Rule::unique('people')->ignore($id, 'id'),
+                ],
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'fail',
+                    'data' => [
+                        'name' => 'required',
+                        'email' => 'required|unique_key',
+                    ]], 400);
+            }
+
+            $people->fill($data)->save();
+            return response()->json(['status' => 'success'], 200);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'no data'], 404);
+    }
+
+    public function delete($id)
+    {
+        $people = People::find($id);
+
+        if (count($people)) {
+            $people->delete();
+            return response()->json(['status' => 'success', 'data' => null], 200);
+        }
+        return response()->json(['status' => 'error'], 404);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api/v1'], function ($router) {
+
+    $router->get('people/{id}', 'PeopleController@show');
+
+    $router->post('people', 'PeopleController@create');
+
+    $router->put('people/{id}', 'PeopleController@update');
+
+    $router->delete('people/{id}', 'PeopleController@delete');
+});
